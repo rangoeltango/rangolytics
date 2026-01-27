961acd2279bbe5e540aa86d3f5293376f0006511
rangoeltango <randy.gowat@gmail.com>
2026-01-26 22:57:20 -0500

    added new individual data to github deploy workflow

--- weekly_update.bat
+++ weekly_update.bat
@@ -1,6 +1,7 @@
 @echo off
 cd C:\Users\randy\OneDrive\Desktop\Rangolytics
 C:\Users\randy\OneDrive\Desktop\Rangolytics\venv\Scripts\python.exe scripts\fetch_fpl.py
+C:\Users\randy\OneDrive\Desktop\Rangolytics\venv\Scripts\python.exe scripts\fetch_current_gw.py
 C:\Users\randy\OneDrive\Desktop\Rangolytics\venv\Scripts\python.exe scripts\build_site.py
 C:\Users\randy\OneDrive\Desktop\Rangolytics\venv\Scripts\python.exe scripts\build_mobile_site.py
 git add .
